59e1b92cbbbd7bbe68189f1cb749a6baef96c77e
Beppo8 <beppo2488sm@gmail.com>
2020-10-31 14:01:20 -0600

    Challenges

---
+++ challenge/dynamic_controls.py
@@ -0,0 +1,41 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class DynamicControls(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path= './chromedriver')
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com/')
+        driver.find_element_by_link_text('Disappearing Elements').click()
+
+    def test_dynamic_controls(self):
+        dirver = self.driver
+
+        checkbox = driver.find_element_by_css_selector("#checkbox > input[type=checkbox]")
+        checkbox.click()
+
+        remove_add_button = driver.find_element_by_css_selector("#check-example > button")
+        remove_add_button.click()
+
+        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#check-example > button")))
+        remove_add_button.click()
+
+        enable_disable_button = driver.find_element_by_css_selector("#input-example > button")
+        enable_disable_button.click()
+
+        WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#input-example > button")))
+
+        text_area = driver.find_element_by_css_selector("#input-example > input[type=text]")
+        text_area.send_keys('Platzi')
+
+        enable_disable_button.click()
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
